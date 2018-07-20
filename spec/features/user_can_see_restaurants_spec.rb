973b7cbbb771a1ce1f209bf7617318a43c7223de
mattb20 <matt_burgoyne@live.co.uk>
2018-07-20 18:20:43 +0100

    failing test for user to see a restaurant logic needs setting upf or passing, query test DB using pg

--- spec/features/user_can_see_restaurants_spec.rb
+++ spec/features/user_can_see_restaurants_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 RSpec.feature "Landing page", type: :feature do
   scenario "User can see they are on yelp" do
     visit("/")
-    expect(page.text).to match(/YELP/)
+    expect(page.text).to match(/YELP/);
+  end
+  scenario "User can see a restaurant" do
+    visit("/")
+    expect(page).to match(/Resturant/);
   end
 end
